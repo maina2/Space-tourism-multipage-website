c90ce0a99fd2dc06fde64365a3b51b181b684687
Simon Chanzu <chanzusimon6@gmail.com>
2023-10-26 23:15:27 +0300

    yeah

--- client/space-tourism-multipage-site/src/pages/Home.jsx
+++ client/space-tourism-multipage-site/src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div className="Home">
       <div className="parapgraph">
-        <p className="so">So you want to travel to space?</p>
+        <p className="so">SO YOU WANT TO TRAVEL TO SPACE?</p>
         <p className="space">SPACE</p>
         <p className="p2">Let’s face it; if you want to go to space, you might as well genuinely go to 
           outer space and not hover kind of on the edge of it. Well sit back, and relax 
